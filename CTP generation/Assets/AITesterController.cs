49cab3df455466766aded65243fd2627f9177bf7
brynfduwe <bryn2.findlay-dykes@live.uwe.ac.uk>
2018-12-18 18:15:50 +0000

    better ai tester - controlled jumps

--- CTP generation/Assets/AITesterController.cs
+++ CTP generation/Assets/AITesterController.cs
@@ -25,6 +25,13 @@ public class AITesterController : MonoBehaviour {
     public Vector2 maxVelocity;
 
     private float jumpInputTimer;
+    private float jumpTime;
+
+    private bool jumpTargetHit = true;
+    private Vector2 jumpTargetPos;
+    private bool stopMoveRight;
+
+    private bool jumpHitIgnore = true;
 
 
     // Use this for initialization
@@ -32,13 +39,14 @@ public class AITesterController : MonoBehaviour {
     {
         GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity / 2;
         GetComponent<Rigidbody2D>().angularVelocity = GetComponent<Rigidbody2D>().angularVelocity / 2;
+
+        jumpTargetPos = transform.position;
     }
 
 
     // Update is called once per frame
     void FixedUpdate()
     {
-
         grounded = Physics2D.OverlapArea(groundPoint1.position, groundPoint2.position);
 
         if (grounded)
@@ -51,43 +59,22 @@ public class AITesterController : MonoBehaviour {
                 hitGround = true;
             }
 
-            RaycastHit2D hitD = Physics2D.Raycast(transform.position + new Vector3(1, 0, 0), -Vector2.up);
+            RaycastHit2D hitD = Physics2D.Raycast(transform.position + new Vector3(0.5f, 0, 0), -Vector2.up);
             if (hitD.collider == null)
             {
-                if (!jumpStarted)
-                {
-                    startedJumpPos = transform.position.y;
-                    //  maxJumpPos = transform.position.y + jumpMax;
-                    GetComponent<Rigidbody2D>().AddForce(Vector2.up * (jumpPower * 1), ForceMode2D.Impulse);
-                    jumpStarted = true;
-                    hitGround = false;
-                }
+                StartJump(0.3f);
             }
 
             RaycastHit2D hitR = Physics2D.Raycast(transform.position + new Vector3(1, 0, 0), Vector2.right, 0.05f);
             if (hitR.collider != null)
             {
-                if (!jumpStarted)
-                {
-                    startedJumpPos = transform.position.y;
-                    //  maxJumpPos = transform.position.y + jumpMax;
-                    GetComponent<Rigidbody2D>().AddForce(Vector2.up * (jumpPower * 1), ForceMode2D.Impulse);
-                    jumpStarted = true;
-                    hitGround = false;
-                }
+                StartJump(0.15f);
             }
 
-            RaycastHit2D hitR2 = Physics2D.Raycast(transform.position + new Vector3(1, 1, 0), Vector2.right, 0.1f);
+            RaycastHit2D hitR2 = Physics2D.Raycast(transform.position + new Vector3(1, 1, 0), Vector2.right, 1f);
             if (hitR2.collider != null)
             {
-                if (!jumpStarted)
-                {
-                    startedJumpPos = transform.position.y;
-                    //  maxJumpPos = transform.position.y + jumpMax;
-                    GetComponent<Rigidbody2D>().AddForce(Vector2.up * (jumpPower * 1), ForceMode2D.Impulse);
-                    jumpStarted = true;
-                    hitGround = false;
-                }
+                StartJump(0.3f);
             }
 
         }
@@ -100,7 +87,7 @@ public class AITesterController : MonoBehaviour {
         {
             jumpInputTimer += Time.deltaTime;
 
-            if (jumpInputTimer < 0.3f)
+            if (jumpInputTimer < jumpTime)
             {
 
                 GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
@@ -127,14 +114,27 @@ public class AITesterController : MonoBehaviour {
             }
         }
 
-        //player wants to end jump
-        if (Input.GetKeyUp(Jump))
+        if (!stopMoveRight && !jumpHitIgnore)
         {
-
+            if (transform.position.x >= jumpTargetPos.x)
+            {
+                stopMoveRight = true;
+            }
+        }
+        else
+        {
+            if (Vector2.Distance(transform.position, jumpTargetPos) < 1)
+            {
+                stopMoveRight = false;
+                jumpTargetHit = true;
+                jumpHitIgnore = true;
+            }
         }
 
-
-        transform.Translate(Vector3.right * Time.deltaTime * speed);
+        if (jumpHitIgnore || !stopMoveRight)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
 
         if (GetComponent<Rigidbody2D>().velocity.x > maxVelocity.x)
             GetComponent<Rigidbody2D>().velocity = new Vector2(maxVelocity.x, GetComponent<Rigidbody2D>().velocity.y);
@@ -143,5 +143,69 @@ public class AITesterController : MonoBehaviour {
         if (GetComponent<Rigidbody2D>().velocity.y > maxVelocity.y)
             GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, maxVelocity.y);
 
+    }
+
+
+    void StartJump(float time)
+    {
+        if (!jumpStarted)
+        {
+            //target platform picker.
+            Collider2D[] cols = Physics2D.OverlapCircleAll(transform.position, 6);
+
+            Transform nearestCol = cols[0].transform;
+            float nearestDist = 100;
+
+            for (int i = 0; i < cols.Length; i++)
+            {
+                if (cols[i].gameObject != this.gameObject)
+                {
+                    //angle check
+                    Vector3 playerDir = cols[i].transform.position - transform.position;
+                    float angle = Vector3.Dot(playerDir, cols[i].transform.right);
+                    if (angle > 0.5f)
+                    {
+                        float dist = Vector2.Distance(transform.position, cols[i].transform.position);
+                        if (dist < nearestDist)
+                        {
+                            nearestDist = Vector2.Distance(transform.position, cols[i].transform.position);
+                            nearestCol = cols[i].transform;
+                        }
+                    }
+                }
+            }
+
+            nearestCol.gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+            jumpTargetPos = nearestCol.position;
+            Debug.Log(nearestDist.ToString());
+
+            //jump set up
+            startedJumpPos = transform.position.y;
+            //  maxJumpPos = transform.position.y + jumpMax;
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * (jumpPower * 1), ForceMode2D.Impulse);
+            jumpStarted = true;
+            hitGround = false;
+
+            jumpTargetHit = false;
+            jumpHitIgnore = false;
+            stopMoveRight = false;
+
+            if (nearestDist < 1)
+            {
+                //short jump
+                jumpTime = 0.1f;
+            }
+            else if(nearestDist < 2.5f)
+            {
+                jumpTime = 0.2f;
+            }
+            else
+            {
+                //highjump
+                jumpTime = 0.3f;
+            }
+
+           // jumpTime = time;
+        }
     }
 }
